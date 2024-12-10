6961c4d2a2034482070c00249a896705d4cfd7ca
ПК <@DESKTOP-7BJGBL9>
2024-12-10 11:45:52 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите количество степенек на лестнеце: ");
+int stairs = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < stairs; i++)
+{
+    for (int j = 0; j < i * 2; j++)
+    {
+        Console.Write(" ");
+    }
+    Console.WriteLine("***");
+
+    for (int j = 0; j < i * 2; j++)
+    {
+        Console.Write(" ");
+    }
+    Console.WriteLine("  *");
+
+}
+
+          
